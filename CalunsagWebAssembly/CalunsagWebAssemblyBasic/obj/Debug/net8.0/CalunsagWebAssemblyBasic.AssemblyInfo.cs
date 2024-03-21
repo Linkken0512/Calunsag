d11115bc829d0f26dade95b32cf9af89abf9cf30
Marc Kenneth I. Calunsag <126436096+Linkken0512@users.noreply.github.com>
2024-03-21 10:55:47 +0800

    123

--- CalunsagWebAssembly/CalunsagWebAssemblyBasic/obj/Debug/net8.0/CalunsagWebAssemblyBasic.AssemblyInfo.cs
+++ CalunsagWebAssembly/CalunsagWebAssemblyBasic/obj/Debug/net8.0/CalunsagWebAssemblyBasic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalunsagWebAssemblyBasic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3baf2868331065ae6fe4792c88e95e8f08f893d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalunsagWebAssemblyBasic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalunsagWebAssemblyBasic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
