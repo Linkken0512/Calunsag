63a6499d977235ec45cc162ea9559f081ca7cdcc
Marc Kenneth I. Calunsag <126436096+Linkken0512@users.noreply.github.com>
2024-05-18 13:02:27 +0800

    database

---
+++ SQLQuery1.sql
@@ -0,0 +1,3 @@
+﻿insert into Products ([Name], Brand, Category,Price,[Description],ImageFileName,CreatedAt)
+values
+('aasdasd','asdsadasd','sadasdasdasd',1234,'dsadasdas','Blackfire_Torch.png',GETDATE())
